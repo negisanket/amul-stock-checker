a072bb4423ea5b444249e439ad10e0b46b6b11bc
negisanket <73462348+negisanket@users.noreply.github.com>
2025-06-22 16:03:00 +0530

    Update check_stock.py

--- check_stock.py
+++ check_stock.py
@@ -39,7 +39,7 @@ def main():
             msg = f"HUURRRRRRRRRay stock available!\n{name}\n🔗 https://shop.amul.com/en/product/{alias_url}"
             send_push_notification("🎉 Stock Available!", msg)
             return
-    send_push_notification("😞 Still Out of Stock", ":( None of the products are available yet.")
+    send_push_notification("SAAAAADDD Still Out of Stock", ":( None of the products are available yet.")
 
 if __name__ == "__main__":
     main()
